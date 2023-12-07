ada10d93854eaff09340ed58b6b2383369e771d4
LegoTed2000 <112373501+LegoTed2000@users.noreply.github.com>
2023-12-07 17:00:35 +1100

    Add files via upload

--- characters_environment.js
+++ characters_environment.js
@@ -53,7 +53,18 @@ var gameConfig={
 =            Game Status             =
 ====================================*/
 
+noseX = "";
+noseY = "";
+GameStatus = "";
+
+function startGame() {
+  GameStatus = "start";
+  document.getElementById("status").innerHTML = "Game is loading ↻";
+}
+
 function game(){
+
+  console.log("noseX = " + noseX + ", noseY = " + noseY);
 
   instializeInDraw();
   moveEnvironment(mario);
@@ -67,7 +78,7 @@ function game(){
     fill(255, 255, 255);
     textSize(40);
     textAlign(CENTER);
-    text("Press Any Arrow Keys to Start and Play ", gameConfig.screenX/2, gameConfig.screenY/2);
+    text("Press Play Button To Start The Game", gameConfig.screenX/2, gameConfig.screenY/2);
     textSize(40);
 
     stroke(255);
@@ -115,7 +126,7 @@ function game(){
 
 // change game status if any key is pressed
 function changeGameStatud(character){
-  if((keyDown(control.up) ||keyDown(control.left)||keyDown(control.right) )&& gameConfig.status==="start") {
+  if(GameStatus =="start" && noseX != "" && gameConfig.status==="start") {
     world_start.play();
     initializeCharacterStatus(mario);
     gameConfig.status= "play";
@@ -279,19 +290,19 @@ function autoControl(character){
 function manualControl(character){
   
   if(character.live){
-    if(keyDown(control.left)){
+    if(keyDown(noseX < 300)){
       character.velocity.x-=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(-1);
     }
 
-    if(keyDown(control.right)){
+    if(keyDown(noseX > 300)){
       character.velocity.x+=gameConfig.moveSpeed;
       character.changeAnimation('move');
       character.mirrorX(1);
     }
 
-    if(!keyDown(control.left)&&!keyDown(control.right)&&!keyDown(control.up)){ 
+    if(!keyDown(noseX < 300)&&!keyDown(noseX > 300)&&!keyDown(noseY < 200)){ 
       character.changeAnimation('stand');
     }
   }
@@ -300,7 +311,7 @@ function manualControl(character){
 
 /* Movements of character */
 function jumping(character){
-	if( (keyWentDown(control.up)&&character.live) || (touchIsDown&&character.live) ){
+	if( (keyWentDown(noseY < 200)&&character.live) || (touchIsDown&&character.live) ){
 		character.velocity.y+=gameConfig.jump;
 	}
 }
